4ecd47df4401c2e54273842af57d014b81c40583
PyotrKam <pyotrk@inbox.lv>
2024-06-27 19:01:24 +0300

    Base for aplication

--- SkillFactory_Module_7.4/Program.cs
+++ SkillFactory_Module_7.4/Program.cs
@@ -8,6 +8,27 @@ using SFML.Learning;
 
         InitWindow(800, 600);
 
+        while (true)
+        {
+            //1. Расчёт
+            DispatchEvents();
+
+            //Игровая логика
+
+
+            //2.Очистка буфера и окно
+
+
+            //3.Отрисовка буфера на окне
+
+            //Вывод методов отрсовки объектов
+            DisplayWindow();
+
+            // 4. Ожидание
+            Delay(1);
+        }
+
+
         Console.ReadLine();
         
         }
